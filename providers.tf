464cec712058404a8e7468485ce38e35e37b6033
fpolido <fpolido14@gmail.com>
2026-06-30 11:11:40 -0300

    Agregado el diagramo y pequeño arreglo en providers.tf

--- providers.tf
+++ providers.tf
@@ -9,10 +9,6 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.23"
     }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.12"
-    }
     tls = {
       source  = "hashicorp/tls"
       version = "~> 4.0"
